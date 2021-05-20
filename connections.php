f4e27f45c19d9b054e2d6606dc29938e36294b63
GI-10836535 <82456010+GI-10836535@users.noreply.github.com>
2021-05-20 20:42:25 +0000

    Update connections.php

--- connections.php
+++ connections.php
@@ -1,13 +1,12 @@
-<?php  
-
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "login_sample_database";
-
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-{
-
-die("failed to connect to database!");
-
-}
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["bdbcc22fc920ea"];
+$cleardb_password = $cleardb_url["9cc6d97a"];
+$cleardb_db = substr($cleardb_url["heroku_9a1153735d811b8"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
